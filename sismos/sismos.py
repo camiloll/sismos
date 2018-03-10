98cdbf6b94a8d2a99250489c0cfc18d11b414f1f
Camilo Londoño L <camiloll@outlook.com>
2018-03-10 12:36:23 -0500

    Minor fix

--- sismos/sismos.py
+++ sismos/sismos.py
@@ -7,9 +7,9 @@ import requests
 
 def lastEarthquake():
     """ Prints last earthquake in Colombia """
-        page = requests.get('http://200.119.88.135/RSNC/paginas/preliminar/detalles.php')
-        tree = html.fromstring(page.content)
-        # Last earthquake in Colombia
-        infoIds = ['fechaL','horaL','mag','orienta','ubica']
-        for id in infoIds:
-                print(tree.xpath('//*[@id="'+id+'"]')[0].text)
+    page = requests.get('http://200.119.88.135/RSNC/paginas/preliminar/detalles.php')
+    tree = html.fromstring(page.content)
+    # Last earthquake in Colombia
+    infoIds = ['fechaL','horaL','mag','orienta','ubica']
+    for id in infoIds:
+            print(tree.xpath('//*[@id="'+id+'"]')[0].text)
